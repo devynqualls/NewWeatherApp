b32d6784023149df57e1a0b8f720b784c1865360
devynqualls <58799637+devynqualls@users.noreply.github.com>
2022-03-28 14:10:04 -0400

    javascript updated

---
+++ app.js
@@ -0,0 +1,25 @@
+let weatherInput = document.getElementById("weatherInput");
+const submitBtn = document.getElementById("submit");
+let weatherDegree = document.getElementById("weatherDegree");
+let weatherLocation = document.getElementById("weatherLocation");
+let weatherIcon = document.getElementById("weatherIcon");
+const Apikey = '156af92f32ba09e4e0287fd07eb7ea36';
+let weatherDesc = document.getElementById("weatherDesc"); 
+
+
+function getWeather(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ weatherInput.value + '&appid=156af92f32ba09e4e0287fd07eb7ea36')
+    .then(res => res.json())
+    .then(data => {
+        weatherLocation.innerHTML = data['name'];
+        weatherDesc.innerHTML = data["weather"][0]['description']
+    })
+
+    .catch(err => alert("No City Found! "))
+}
+
+submitBtn.addEventListener("click", getWeather())
+
+
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+//156af92f32ba09e4e0287fd07eb7ea36
